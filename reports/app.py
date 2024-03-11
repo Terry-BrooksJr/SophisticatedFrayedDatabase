1a6fef8eaab5a22dc154ea439f75b5a9d089ef7e
Terry Arthur Brooks, Jr <Terry.Arthur@BrooksJr.com>
2024-03-11 15:05:25 -0400

    Update app.py

--- reports/app.py
+++ reports/app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template
 import os
-from reports.utils  import sign_url, current_time
+from utils  import sign_url, current_time
 import time
 
 def create_app(test_config=None):
@@ -30,7 +30,7 @@ def create_app(test_config=None):
     @app.route("/home")
     @app.route("/index")    
     def index():
-        return render_template('report.html', signed_url=sign_url(url=f'https://app.mode.com/solutionssandbox/reports/663318dbd45e/embed?access_key={os.getenv("ACCESS_KEY")}&max_age=2629800&timestamp={current_time}', key=os.getenv('ACCESS_KEY'), secret=os.getenv('ACCESS_SECRET')
+        return render_template('report.html', signed_url=sign_url(url=f'https://app.mode.com/solutionssandbox/reports/663318dbd45e/embed?access_key={os.getenv("ACCESS_KEY")}&max_age=2629800&secret_key={os.getenv("MODE_SECRET_KEY")}&timestamp={current_time}', key=os.getenv('ACCESS_KEY'), secret=os.getenv('ACCESS_SECRET')
                                                                   )
                                )
     return app
